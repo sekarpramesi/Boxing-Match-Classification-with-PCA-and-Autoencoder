24daefb92d01b0dc1beb29d33116af8ad435765d
Niya <niyasekar@gmail.com>
2018-06-25 08:11:31 +0700

    classifier done

--- boxing_classifier.py
+++ boxing_classifier.py
@@ -3,21 +3,21 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier as mlp
+from sklearn.neural_network import MLPClassifier as mlx
 from sklearn.metrics import classification_report,confusion_matrix
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.neural_network import MLPRegressor as mlp
 
 def init(df):
     #to_drop=['judge1_A', 'judge1_B', 'judge2_A', 'judge2_B', 'judge3_A','judge3_B']
     #df.drop(to_drop,inplace=True,axis=1)
-    df['result'] = df['result'].map({'draw': 0, 'win_A': 1,'win_B':2})
     df['stance_A']=df['stance_A'].map({'orthodox':0,'southpaw':1})
     df['stance_B']=df['stance_B'].map({'orthodox':0,'southpaw':1})
-    df['decision']=df['decision'].map({'SD':1,'MD':2,'UD':3,'KO':4,'TKO':5,'DQ':6,'RTD':7})
     return df
 
-def removeNan(df):
-    df.fillna(-1,inplace=True)
+def removeNan(df,x):
+    df.fillna(x,inplace=True)
+    res = df.values
     return df
 
 def removeRows(df,t):
@@ -28,12 +28,10 @@ def removeColumns(df,x):
     df.drop(x,inplace=True,axis=1)
     return df
 
-def normalize(df):
-    x=df.values
-    min_max_scaler=preprocessing.MinMaxScaler()
+def normalize(x):
+    min_max_scaler=preprocessing.StandardScaler()
     x_scaled=min_max_scaler.fit_transform(x)
-    df=pd.DataFrame(x_scaled,columns=df.columns)
-    return df
+    return x_scaled
 
 def generate(df,name):
     df.to_csv(name,encoding='utf-8',index=False,sep=',')
@@ -42,46 +40,64 @@ def separate(df):
     features = ['age_A', 'age_B', 'height_A', 'height_B', 'reach_A', 'reach_B',\
        'stance_A', 'stance_B', 'weight_A', 'weight_B', 'won_A', 'won_B',\
        'lost_A', 'lost_B', 'drawn_A', 'drawn_B', 'kos_A', 'kos_B',\
-       'decision', 'judge1_A', 'judge1_B', 'judge2_A', 'judge2_B', 'judge3_A',\
+       'judge1_A', 'judge1_B', 'judge2_A', 'judge2_B', 'judge3_A',\
        'judge3_B']
+    t1= ['result']
+    t2=['decision']
     dx = df.loc[:,features].values
-    dy = df.loc[:,['result']].values
+    dy1 = df.loc[:,t1].values
+    dy2 = df.loc[:,t2].values
     x = pd.DataFrame(dx,columns=features)
-    y = pd.DataFrame(dy,columns=['result'])
+    y1 = pd.DataFrame(dy1,columns=t1)
+    y2 = pd.DataFrame(dy2,columns=t2)
+    y1= y1.iloc[:,0].str.replace(' ','').str.get_dummies(sep=',')
+    y2= y2.iloc[:,0].str.replace(' ','').str.get_dummies(sep=',')
+    print(y1.head(5))
+    print(y2.head(5))
+    y  = pd.concat([y1,y2], axis=1)
+    print(y.head(5))
     return x,y
 
 def split(x,y):
-    xtr,xte,ytr,yte=train_test_split(x,y,test_size=0.2,random_state=0)
+    xtr,xte,ytr,yte=train_test_split(x,y,test_size=0.1,random_state=0)
     return xtr,xte,ytr,yte
     
 
 def pca(df1,df2,n):
     pca = PCA(n)
     pca.fit(df1)
-    x = pca.transform(df1)
-    y = pca.transform(df2)
-    return x,y
+    x1 = pca.transform(df1)
+    x2 = pca.transform(df2)
+    return x1,x2
 
-def runner(x,y):
+def runner(x):
    x,y = separate(x)
-   x = removeNan(x)
-   y = removeNan(y)
+   x = removeNan(x,-1)
+   y = removeNan(y,0)
    xtr,xte,ytr,yte = split(x,y)
+   xtr = xtr.values
+   xte = xte.values
+   ytr = ytr.values
+   yte = yte.values
    xtr = normalize(xtr)
    xte = normalize(xte)
    xtr,xte = pca(xtr,xte,.95)
    return xtr,xte,ytr,yte
 
 def classify(x1,x2,y1,y2):
-    clf = mlp(hidden_layer_sizes=(13,13,13),solver='sgd',\
-                       learning_rate_init=0.01,max_iter=500)
-    clf.fit(x1,y1.values.ravel())
+    clf = mlx(solver='adam', alpha=1e-5,\
+                   hidden_layer_sizes=(13,13,13), random_state=1,activation='relu')
+    clf.fit(x1,y1)
     predictions = clf.predict(x2)
-    print(confusion_matrix(y2.values.ravel(),predictions,labels=[0,1,2]))
-    print(classification_report(y2,predictions,target_names=['draw','win_A','win_B']))
-    
-#textName = input("Type filename here : ")
-df=pd.read_csv("usethis.csv")
+    cols=['draw','win_A','win_B','DQ','KO','MD','NWS','PTS','RTD','SD','TD','TKO','UD'];
+    res = pd.DataFrame(predictions,columns=cols)
+    return res
+
+df=pd.read_csv("boxing.csv")
+df=init(df)
 x = y = xtr = xte = ytr = yte = df.copy()
-xtr,xte,ytr,yte = runner(x,y)
-classify(xtr,xte,ytr,yte)
+xtr,xte,ytr,yte = runner(x)
+res = classify(xtr,xte,ytr,yte)
+print(res.loc[res['draw'] == 1])
+print(res.loc[res['win_A'] == 1])
+print(res.loc[res['win_B'] == 1])
